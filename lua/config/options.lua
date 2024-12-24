cf6e50fe64cf44b373d2910e007e9c4a315b874a
Justin Luque <justin.luque@gmail.com>
2024-12-24 13:26:15 -0500

    feat: create msys2 clipboard support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.clipboard = {
+  name = "msys2-clipboard",
+  copy = {
+    ["+"] = "C:\\msys64\\usr\\bin\\bash.exe -c '/usr/local/bin/xclip'",
+    ["*"] = "C:\\msys64\\usr\\bin\\bash.exe -c '/usr/local/bin/xclip'",
+  },
+  paste = {
+    ["+"] = "C:\\msys64\\usr\\bin\\bash.exe -c '/usr/local/bin/xclip -o'",
+    ["*"] = "C:\\msys64\\usr\\bin\\bash.exe -c '/usr/local/bin/xclip -o'",
+  },
+  cache_enabled = false,
+}
