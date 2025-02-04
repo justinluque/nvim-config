a201008d41e394b78b7050f6324397c562509e8b
Justin Luque <justin.luque@gmail.com>
2025-02-04 12:06:00 -0500

    feat: change color scheme
    me likey

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
   -- add catppuccin
-  { "catppuccin/nvim" },
+  { "AlessandroYorba/Alduin" },
 
   -- Configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "Alduin",
     },
   },
 }
